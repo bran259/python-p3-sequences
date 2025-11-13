7dd51ab222b7b1ab10003e71fc3f96d9fea04cfe
bran259 <kiprop686@gmail.com>
2025-11-13 15:20:13 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = []
+
+    if length == 0:
+        print(sequence)
+        return
+    
+    elif length == 1:
+        sequence = [0]
+        print(sequence)
+        return
+    
+    sequence = [0, 1]
+    while len(sequence) < length:
+        next_num = sequence[-1] + sequence[-2]
+        sequence.append(next_num)
+
+    print(sequence)    
     pass
+
+# def print_fibonacci(n):
+#     if n <= 0:
+#         print([])
+#         return
+
+#     fib = [0, 1]
+#     while len(fib) < n:
+#         fib.append(fib[-1] + fib[-2])
+
+#     print(fib[:n])
